8285bae1f2b2bab5eb3d50f747780d91f64982d6
Rahmat Hidayat <rahmathidayatxtki5@gmail.com>
2023-11-14 08:19:35 +0700

    Documentasi API get

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -27,9 +27,49 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-// Service
-Route::get('/services', [ServiceController::class, 'getServices']);
-Route::get('/services/by-name', [ServiceController::class, 'getServicesByName']);
+
+
+
+
+//! SERVIES
+Route::get('/service', [ServiceController::class, 'service'])->name('service');
+Route::get('/service', [ServiceController::class, 'showservice'])->name('service');
+Route::get('/show-service', [ServiceController::class, 'showService'])->name('show-service');
+//add service
+Route::get('/service/create', [ServiceController::class, 'create'])->name('service-create');
+//edit service
+Route::get('service/{id}/edit', [ServiceController::class, 'edit'])->name('service-edit');
+
+//! CAREER
+// show career
+Route::get('/career', [CareerController::class, 'career'])->name('career');
+Route::get('/career', [CareerController::class, 'showcareer'])->name('career');
+Route::get('/show-career', [CareerController::class, 'showCareer'])->name('show-career');
+// Add career
+Route::get('/career/create', [CareerController::class, 'create'])->name('career-create');
+// edit & update career
+Route::get('career/{id}/edit', [CareerController::class, 'edit'])->name('career-edit');
+
+
+
+        Route::get('/career/{id}/edit', [CareerController::class, 'edit'])->name('career-edit');
+        Route::get('/career/jobQualification', [JobQualificationController::class, 'getJobQualification'])->name('career.get-jobQualification');
+        Route::get('/career/jobQualification/add', [JobQualificationController::class, 'addJobQualification'])->name('career.add-jobQualification');
+        Route::get('/pages', [PagesController::class, 'pagesShow'])->name('pages');
+        Route::get('/page/about', [PagesController::class, 'previewAbout'])->name('preview-about');
+        Route::get('/page/home', [PagesController::class, 'previewHome'])->name('preview-home');
+        Route::get('/page/service', [PagesController::class, 'previewService'])->name('service-pages.index');
+        Route::get('/adminpanel/todolist', [ToDoListController::class, 'getToDoList'])->name('todolist.index');
+        Route::get('/adminpanel/todolist/{todolist}/edit', [ToDoListController::class, 'editToDoList'])->name('adminpanel.todolist.edit');
+        Route::get('/user', [UserController::class, 'user'])->name('user');
+        Route::get('/user/edit/{id}', [UserController::class, 'edit'])->name('user.edit');
+        Route::get('/register', [RegisterController::class, 'registerpage'])->name('register');
+
+
+
+
+
+
 
 // Service Page
 Route::get('/service-pages', [ServiceController::class, 'getServicePages']);
